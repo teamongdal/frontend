303d361241267c138d1d61b0526cb709e2ef1120
jeongtunjang <jjy306105@gmail.com>
2025-02-18 11:40:22 +0900

    feat: VideoDetailPage '유사상품 찾기' 클릭시 녹음, '녹음 중지' 클릭시 유사상품 조회 api req 진행

--- src/components/page/VideoDetailPage.js
+++ src/components/page/VideoDetailPage.js
@@ -6,10 +6,12 @@ import {
   TouchableOpacity,
   Image,
   Dimensions,
+  Button,
 } from "react-native";
 import VideoPlayer from "../molecule/VideoPlayer";
 import ViewShot, { captureRef } from "react-native-view-shot";
 import { useNavigation } from "@react-navigation/native";
+import { Audio } from "expo-av";
 import axios from "axios";
 
 const { width, height } = Dimensions.get("window");
@@ -23,6 +25,8 @@ const VideoDetailPage = ({ route }) => {
   const videoRef = useRef(null);
   const viewShotRef = useRef(null);
   const navigation = useNavigation();
+  const [recording, setRecording] = useState(null);
+  const [permissionResponse, requestPermission] = Audio.usePermissions();
 
   useEffect(() => {
     if (!videoId) return;
@@ -39,13 +43,112 @@ const VideoDetailPage = ({ route }) => {
       });
   }, [videoId]);
 
+  const handleClickSearch = () => {
+    if (videoRef.current) {
+      videoRef.current.pause();
+      setIsPlaying(() => (isPlaying ? !isPlaying : isPlaying));
+    }
+
+    startRecording();
+  };
+
+  async function startRecording() {
+    try {
+      if (permissionResponse.status !== "granted") {
+        console.log("Requesting permission..");
+        await requestPermission();
+      }
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+
+      console.log("Starting recording..");
+      const recording = new Audio.Recording();
+      await recording.prepareToRecordAsync({
+        isMeteringEnabled: false,
+        android: {
+          extension: ".wav",
+          outputFormat: Audio.RECORDING_OPTION_ANDROID_OUTPUT_FORMAT_DEFAULT,
+          audioEncoder: Audio.RECORDING_OPTION_ANDROID_AUDIO_ENCODER_AMR_NB,
+          sampleRate: 44100,
+          numberOfChannels: 1, // 모노 설정
+          bitRate: 128000,
+        },
+        ios: {
+          extension: ".wav",
+          outputFormat: Audio.RECORDING_OPTION_IOS_OUTPUT_FORMAT_LINEARPCM,
+          audioQuality: Audio.RECORDING_OPTION_IOS_AUDIO_QUALITY_HIGH,
+          sampleRate: 44100,
+          numberOfChannels: 1, // 모노 설정
+          bitRate: 128000,
+        },
+      });
+
+      await recording.startAsync();
+      setRecording(recording);
+      console.log("Recording started");
+
+      handleCapture();
+    } catch (err) {
+      console.error("Failed to start recording", err);
+    }
+  }
+
+  async function stopRecording() {
+    console.log("Stopping recording..");
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+    });
+    const uri = recording.getURI();
+    console.log("Recording stopped and stored at", uri);
+    sendAudioToServer(uri);
+  }
+
+  const sendAudioToServer = async (uri) => {
+    try {
+      let fileBlob = null;
+      const formData = new FormData();
+
+      const imageFile = Image.resolveAssetSource(
+        require("../../assets/video/highlight_0001_0001.png")
+      ).uri;
+
+      // 이미지 파일 추가
+      formData.append("image", {
+        uri: imageFile,
+        name: "captured_image.png",
+        type: "image/png",
+      });
+
+      const fileType = uri.endsWith(".wav") ? "audio/wav" : "audio/m4a"; // iOS의 경우 확장자 확인
+      formData.append("audio", {
+        uri: uri.startsWith("file://") ? uri : `file://${uri}`,
+        name: `recorded_audio.${fileType === "audio/wav" ? "wav" : "m4a"}`, // iOS 확장자 확인
+        type: fileType,
+      });
+
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/search_product?user_id=user_0001",
+        formData,
+        {
+          headers: { "Content-Type": "multipart/form-data" },
+        }
+      );
+
+      console.log("Response from server:", response.data);
+    } catch (error) {
+      console.error(
+        "Error sending audio:",
+        error.response?.data || error.message
+      );
+    }
+  };
+
   const handleCapture = async () => {
     try {
-      if (videoRef.current) {
-        videoRef.current.pause();
-        setIsPlaying(() => (isPlaying ? !isPlaying : isPlaying));
-      }
-
       const uri = await captureRef(viewShotRef, {
         format: "png",
         quality: 0.8,
@@ -75,8 +178,22 @@ const VideoDetailPage = ({ route }) => {
           />
         )}
       </ViewShot>
-
-      <TouchableOpacity style={styles.captureButton} onPress={handleCapture}>
+      <View style={{ flexDirection: "row" }}>
+        {/* <Button
+          title="녹음 시작"
+          onPress={startRecording}
+          disabled={!!recording}
+        /> */}
+        <Button
+          title="녹음 종료"
+          onPress={stopRecording}
+          disabled={!recording}
+        />
+      </View>
+      <TouchableOpacity
+        style={styles.captureButton}
+        onPress={handleClickSearch}
+      >
         <Text style={styles.buttonText}>유사상품 찾기</Text>
       </TouchableOpacity>
 
