64a7bbfb911d462aea8494eb100d50e380e73592
jeongtunjang <jjy306105@gmail.com>
2025-02-19 17:16:51 +0900

    feat: HighlightPage 추가

--- src/components/molecule/ProductInfo.js
+++ src/components/molecule/ProductInfo.js
@@ -1,8 +1,82 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 
-const ProductInfo = () => {
-  return <></>;
+const ProductInfo = ({ item }) => {
+  return (
+    <View style={styles.productContainer}>
+      <Image
+        source={{ uri: item.product_image_url }}
+        style={styles.productImage}
+      />
+      <View style={styles.productInfo}>
+        <Text style={styles.brand}>{item.brand_name}</Text>
+        <View style={styles.categoryTag}>
+          <Text style={styles.categoryText}>{item.category}</Text>
+        </View>
+        <Text style={styles.productTitle}>{item.product_name}</Text>
+        <Text style={styles.discount}>
+          {item.discount_rate == 0 ? "" : `${item.discount_rate}`}
+        </Text>
+        <Text style={styles.price}>{item.final_price} 원</Text>
+      </View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  productContainer: {
+    flexDirection: "row",
+    flex: 1.2,
+    backgroundColor: "#fff",
+    padding: 15,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+  },
+  productImage: {
+    width: 200,
+    height: 200,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  productInfo: {
+    flex: 1,
+  },
+  brand: {
+    fontSize: 14,
+    fontWeight: "bold",
+    color: "#888",
+    marginBottom: 2,
+  },
+  categoryTag: {
+    backgroundColor: "#6C3641",
+    paddingVertical: 3,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    alignSelf: "flex-start",
+    marginBottom: 5,
+  },
+  categoryText: {
+    fontSize: 12,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  productTitle: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  discount: {
+    fontSize: 14,
+    color: "red",
+    fontWeight: "bold",
+  },
+  price: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
 
 export default ProductInfo;
--- src/components/page/HighLightPage.js
+++ src/components/page/HighLightPage.js
@@ -1,22 +1,33 @@
-import React, { useEffect, useState } from "react";
-import {
-  View,
-  Text,
-  Image,
-  StyleSheet,
-  ActivityIndicator,
-  FlatList,
-  Dimensions,
-} from "react-native";
-import highlightImage from "../../assets/video/highlight_0001_0001.png";
-const { width } = Dimensions.get("window"); // 화면 크기 가져오기
-const SERVER_URL = "http://127.0.0.1:8000"; // 백엔드 서버 주소
+import React, { useState, useEffect } from "react";
+import { View, Text, Image, StyleSheet, FlatList } from "react-native";
+import ProductInfo from "../molecule/ProductInfo";
 
-const HighlightPage = () => {
+const renderItem = ({ item }) => {
+  return (
+    <View style={styles.container}>
+      {/* 영상 장면 */}
+      <View style={styles.videoContainer}>
+        <Image
+          source={{ uri: item.highlight_image_url.replace(/^C:\//, "/") }}
+          style={styles.videoImage}
+        />
+      </View>
+
+      {/* 상품 정보 */}
+      <ProductInfo item={item} />
+    </View>
+  );
+};
+
+const HighlightScreen = () => {
   const [highlightData, setHighlightData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const productImage =
-    "https://ai-shop-bucket.s3.ap-southeast-2.amazonaws.com/recommendations/images/product_images/blazer_0009_00.jpg";
+  const [isLoading, setIsLoading] = useState(false);
+
+  const SERVER_URL = "http://127.0.0.1:8000"; // 백엔드 서버 주소
+
+  useEffect(() => {
+    console.log("highlightData: ", highlightData);
+  }, [highlightData]);
 
   useEffect(() => {
     fetch(`${SERVER_URL}/api/all_product_list?video_id=video_0001`)
@@ -24,7 +35,7 @@ const HighlightPage = () => {
       .then((data) => {
         console.log("data: ", data);
         setHighlightData(data.all_product_list);
-        setLoading(false);
+        setIsLoading(false);
       })
       .catch((error) => {
         console.error("API 요청 실패:", error);
@@ -32,100 +43,54 @@ const HighlightPage = () => {
       });
   }, []);
 
-  if (loading) {
-    return (
-      <View style={styles.loadingContainer}>
-        <ActivityIndicator size="large" color="#fff" />
-      </View>
-    );
-  }
-
   return (
-    <View style={styles.container}>
+    <View style={styles.screen}>
+      <Text style={styles.header}>하이라이트</Text>
       <FlatList
         data={highlightData}
-        keyExtractor={(item) => item.highlight_idx.toString()}
-        renderItem={({ item }) => (
-          <View style={styles.highlightContainer}>
-            {/* 왼쪽: 주요 장면 */}
-            <Image
-              source={highlightImage}
-              style={[
-                styles.sceneImage,
-                { width: width * 0.35, height: width * 0.2 },
-              ]} // 반응형 크기
-            />
-
-            {/* 오른쪽: 상품 정보 */}
-            <View style={styles.productContainer}>
-              <Image
-                source={{ uri: productImage }}
-                style={[
-                  styles.productImage,
-                  { width: width * 0.15, height: width * 0.15 },
-                ]}
-              />
-              <View style={styles.textContainer}>
-                <Text style={styles.brand}>{item.brand_name}</Text>
-                <Text style={styles.productName} numberOfLines={1}>
-                  {item.product_name}
-                </Text>
-                <Text style={styles.price}>{item.final_price}</Text>
-              </View>
-            </View>
-          </View>
-        )}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
       />
     </View>
   );
 };
+
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: "#000",
+    padding: 20,
+  },
+  header: {
+    fontSize: 26,
+    fontWeight: "bold",
+    color: "#fff",
+    marginBottom: 20,
+  },
   container: {
-    flex: 1,
-    backgroundColor: "#2E4A41",
-    padding: 16,
-  },
-  loadingContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#2E4A41",
-  },
-  highlightContainer: {
     flexDirection: "row",
     alignItems: "center",
     marginBottom: 20,
+    borderRadius: 15,
+    padding: 10,
   },
-  sceneImage: {
+  videoContainer: {
+    flex: 1,
+    marginRight: 10,
+  },
+  videoImage: {
+    width: "100%",
+    height: 334,
     borderRadius: 10,
   },
-  productContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginLeft: 20,
-    flex: 1,
+  itemImage: {
+    width: 70,
+    height: 70,
+    marginRight: 10,
+    borderRadius: 8,
+    backgroundColor: "#eee",
   },
-  productImage: {
-    borderRadius: 10,
-    marginRight: 16,
-  },
-  textContainer: {
-    flex: 1,
-  },
-  brand: {
-    fontSize: width * 0.018, // 가변적인 폰트 크기
-    fontWeight: "bold",
-    color: "#D4A017",
-  },
-  productName: {
-    fontSize: width * 0.016,
-    color: "#EDEDED",
-    marginVertical: 4,
-  },
-  price: {
-    fontSize: width * 0.02,
-    fontWeight: "bold",
-    color: "#FFFFFF",
-  },
+  itemDetails: { flex: 1 },
 });
-export default HighlightPage;
+
+export default HighlightScreen;
