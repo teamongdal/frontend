e741d38ede60422066fc1fa8e49c758da0f764f0
jeongtunjang <jjy306105@gmail.com>
2025-02-22 13:57:41 +0900

    feat: 미사용 페이지 제거 CartPage, ProductList, ProductListTest2Page

--- App.js
+++ App.js
@@ -3,13 +3,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { View, Text } from "react-native";
-import CartPage from "./src/components/page/CartPage";
 import HighLightPage from "./src/components/page/HighLightPage";
 import WishlistPage from "./src/components/page/WishlistPage";
 import VideoDetailPage from "./src/components/page/VideoDetailPage";
 import VideoListPage from "./src/components/page/VideoListPage";
-// import TestAPIPage from "./src/components/page/TestAPIPage";
-import ProductListTest2Page from "./src/components/page/ProductListTest2Page";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -20,8 +17,6 @@ const TabNavigator = () => {
       <Tab.Screen name="VideoList" component={VideoListPage} />
       <Tab.Screen name="WishlistPage" component={WishlistPage} />
       <Tab.Screen name="HighLightPage" component={HighLightPage} />
-      {/* <Tab.Screen name="Cart" component={CartPage} /> */}
-      {/* <Tab.Screen name="ProductListTest2" component={ProductListTest2Page} /> */}
     </Tab.Navigator>
   );
 };
@@ -37,12 +32,7 @@ const App = () => {
         />
         <Stack.Screen name="VideoDetail" component={VideoDetailPage} />
         <Stack.Screen name="HighLightPage" component={HighLightPage} />
-        {/* <Stack.Screen name="WishlistPage" component={WishlistPage} /> */}
-        {/* <Stack.Screen name="Cart" component={CartPage} /> */}
-        <Stack.Screen
-          name="ProductListTest2"
-          component={ProductListTest2Page}
-        />
+        <Stack.Screen name="WishlistPage" component={WishlistPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
